cfdacb5990d4c61d91526eb623d875b6b5ac14e8
Mogoson <mogoson@qq.com>
2018-04-04 22:56:45 +0800

    Modify namespace.

--- Assets/MeshCombiner/Editor/MeshCombiner.cs
+++ Assets/MeshCombiner/Editor/MeshCombiner.cs
@@ -14,7 +14,7 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-namespace Developer.MeshCombiner
+namespace Mogoson.MeshCombiner
 {
     public class MeshCombiner : ScriptableWizard
     {
